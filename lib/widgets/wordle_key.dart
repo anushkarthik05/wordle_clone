8763817184bd2b512842a262ecb93c912d6ac168
Anush Karthik <anushkarthik@Anushs-MacBook-Air.local>
2022-01-22 22:11:37 +0530

    UI done

---
+++ lib/widgets/wordle_key.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WordleKey extends StatelessWidget {
+  final String letter;
+  const WordleKey(this.letter, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget keyCap;
+    double width =60;
+    if(letter == "_"){
+      keyCap=Icon(Icons.keyboard_return, size: 18,);
+    }
+    else if(letter=="<"){
+      keyCap=Icon(Icons.backspace_outlined, size: 18,);
+    }
+    else{
+      width = 40;
+      keyCap=Text(letter, style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold));
+    }
+
+    return Container(
+      width: width,
+      height: 60,
+      alignment: Alignment.center,
+      //padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(4)),
+        //border: Border.all(color: Colors.grey, width: 2),
+        color: Color.fromARGB(44,44,44,44),
+      ),
+      child: keyCap,
+    );
+  }
+}
---
+++ lib/widgets/wordle_title_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WordleTitleBar extends StatelessWidget {
+  const WordleTitleBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("WORDLE",style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),),
+        ],
+      ),
+    );
+  }
+}
